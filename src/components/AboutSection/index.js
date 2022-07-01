b4a86e4e285ec435c71025750748cb6df6b350d9
alicekres <alice.kreslins@gmail.com>
2022-07-01 21:27:52 +0100

    change margin at AboutSection to allow logo float and change some paragraph text

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -10,29 +10,40 @@ const AboutSection = () => {
                     I was born and raised in Estonia. As I loved to take care of
                     people and bring them joy - I decided to study nursing.
                     After finishing studies I moved to England where I worked as
-                    a nurse for a time.
+                    a nurse for a little while.
                 </p>
                 <p>
                     However, my husband who works as a front-end developer
                     sparked an interest in me for programming. After finishing a
-                    task he often shared with me a little bit of code which I
-                    didn't understand at all at the time.That excitement got me
-                    curious and I started to find out and trying to understand
-                    the concept of programming. At first it was just for fun but
-                    as the time went on I realized that I'd like to try study
-                    programming more seriously.
+                    task he often shared with me a little bit of code. That
+                    excitement got me curious and I started to find out more. At
+                    first it was just for fun but as the time went on I realized
+                    that I'd like to study programming more seriously.
                 </p>
                 <p>
-                    I started with simple courses in youtube by netninja about
-                    css, html, and javascript. After that I took on courses at
-                    Udemy and built my very first project based on the course by
-                    Maximilian Shwartzmüller using JavaScript, React and NextJs.
-                    The following step to that was starting my studies at
-                    futureproof where we are covering large area of technologies
-                    and most importantly simulate the working environment, using
+                    I started with simple courses in YouTube by The Net Ninja
+                    about HTML, CSS and JavaScript. After that I took on courses
+                    at Udemy and built my very first project based on the course
+                    by Maximilian Shwartzmüller using JavaScript, React and
+                    NextJs. This first project was challenging for me as
+                    everything I did was new and at times confusing. However, I
+                    liked the process to create my own page. That project
+                    allowed me to attain greater knowledge and understanding of
+                    what it means to actually sit behind computer and solve
+                    issues, to implement creativity and having patience to
+                    finish the project.
+                </p>
+
+                <p>
+                    The next step for me was to leave independent studies at
+                    home and move on and try working with others. I was able to
+                    find an amazing opportunity at futureproof where we are
+                    covering many programming technologies and languages like
+                    JavaScript, React, Redux, Docker to name the few. Most
+                    importantly we are simulating the working environment, using
                     agile working environment with stand-ups, retros,
-                    collaboration and solving and/or trying to avoid merging
-                    conflicts in Github.
+                    collaboration and using tools like Git/Github, KanBan boards
+                    and others.
                 </p>
 
                 <p>
